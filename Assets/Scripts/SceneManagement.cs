af2da2dfa6bb81d70a46c7cab5bf8b85333fbf2a
Marco Ramirez <marco.ramirez@my.unitec.edu.mx>
2021-01-27 11:35:14 -0600

    Initial Commit. Main Menu and Load Game menu

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("MainLoop");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("LoadGame");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            MainMenu();
+        }
+    }
+}
