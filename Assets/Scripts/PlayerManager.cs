481e6d8e76ba577def18e41e641a4ab90f89886e
Marco Ramirez <marco.ramirez@rzerocorp.com>
2021-01-30 17:18:58 -0600

    Added Health Bar. Started implementation.

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,9 +6,18 @@ public class PlayerManager : MonoBehaviour
 {
     public CharacterController2D controller;
     public float runSpeed = 40f;
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthBar = healthBar;
+
     float horizontalMove = 0f;
     bool jump = false;
     bool crouch = false;
+
+    void Start() {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
 
     // Update is called once per frame
     void Update()
@@ -28,11 +37,24 @@ public class PlayerManager : MonoBehaviour
         {
             crouch = false;
         }
+
+        // Test damage
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            TakeDamage(20);
+        }
+
     }
 
     private void FixedUpdate()
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
     }
 }
